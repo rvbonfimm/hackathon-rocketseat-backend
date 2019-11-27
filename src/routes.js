60c067f3260fad0a25e52da75acd3b5b18e5f149
rvbonfimm <rogeriovilelabonfim@gmail.com>
2019-11-27 10:33:20 -0300

    Add test route

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,10 @@ routes.get('/', (req, res) => {
   res.json({ ok: true })
 })
 
+routes.get('/api/home', (req, res) => {
+  res.json({ message: 'hello, world!!!' });
+})
+
 routes.get('/api/contact', FormController.index)
 
 routes.post('/api/contact', FormController.store)
